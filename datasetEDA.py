b3b8566dd9a0e1d15e15caaadeb2e798d0301d69
Jonathan A. Yepez Montenegro <jonathanyepezm@gmail.com>
2021-05-09 19:03:10 -0500

    Feature Information
    Implemented code to show the most relevant features.
    Included PEP8 formatting in code.

--- datasetEDA.py
+++ datasetEDA.py
@@ -2,32 +2,34 @@
 """
 Created on Thu May  6 19:41:41 2021
 
-@author: Jonathan A. Yepez M. 
+@author: Jonathan A. Yepez M.
 """
 
-#Script to process data regarding Attrition based on IBM dataset
+# Script to process data regarding Attrition based on IBM dataset
 
-#Import relevant libraries
+# Import relevant libraries
 import pandas as pd
-#import numpy as np
-#import matplotlib
+# import numpy as np
+# import matplotlib
 import matplotlib.pyplot as plt
-#import seaborn as sns #already imported in 'processing'
-import missingno #not 1005 sure if we will use this one
+# import seaborn as sns #already imported in 'processing'
+import missingno  # not 1005 sure if we will use this one
 import processingAttributes as processing
+from sklearn.model_selection import train_test_split
 
-#Preliminary Data Processing
-df = pd.read_csv('EmployeeAttrition.csv') #read the file that is in the folder
-print(df.info()) #general information about the dataframe
-print(df.head()) #first 3 entries in the dataframe
+# Preliminary Data Processing
+df = pd.read_csv('EmployeeAttrition.csv')  # read the file from the folder
+# print(df.info()) #general information about the dataframe
+# print(df.head()) #first 3 entries in the dataframe
 
-#we have only one value in the column EmployeeCount. We can basically delete it
+# we have only one value in the column EmployeeCount. We can delete it
 df.drop('EmployeeCount', inplace=True, axis=1)
-df.drop('Over18', inplace=True, axis=1) #all employees are assumed to be over18
-df.drop('EmployeeNumber', inplace=True, axis=1) #get rid of the employee ID
+df.drop('Over18', inplace=True, axis=1)  # all employees assumed to be over18
+df.drop('EmployeeNumber', inplace=True, axis=1)  # get rid of the employee ID
+df.drop('StandardHours', inplace=True, axis=1)  # column has only one value: 80
+standardHrs = 80
 
-
-#Specify our categorical variables as 'category'
+# Specify our categorical variables as 'category'
 df['Attrition'] = df['Attrition'].astype('category')
 df['BusinessTravel'] = df['BusinessTravel'].astype('category')
 df['Department'] = df['Department'].astype('category')
@@ -36,32 +38,72 @@ df['OverTime'] = df['OverTime'].astype('category')
 df['EducationField'] = df['EducationField'].astype('category')
 df['JobRole'] = df['JobRole'].astype('category')
 df['MaritalStatus'] = df['MaritalStatus'].astype('category')
+# copy of categorical features
+df_categs = df.select_dtypes(include=['category']).copy()
 
+
+# use label encoding to change data from categorical to int8 in the dataframe
+# Encode categorical vars so that we can use feature selection algorithms
+categorical_features = ['BusinessTravel', 'Department', 'Gender', 'OverTime',
+                        'EducationField', 'JobRole', 'MaritalStatus']
+
+for f in categorical_features:
+    colname = f+'_cat'
+    df[colname] = df[f].cat.codes
+    df.drop(f, axis=1, inplace=True)
+
+df['Attrition'] = df['Attrition'].cat.codes  # change yes/no to 1/0
+
+del f, colname, categorical_features
 
 tempVar = df['Attrition']
-df.drop('Attrition', axis=1,inplace = True)
-df.insert(0, 'Attrition', tempVar) #moving our variable of interest to the first column in the df
-del tempVar #delete the temporary variable to prevent cluttering our workspace
+df.drop('Attrition', axis=1, inplace=True)
+df.insert(0, 'Attrition', tempVar)  # move target to the first column in the df
+del tempVar  # delete the temporary variable
 
-#Checking for null values, empty cells
+# Checking for null values, empty cells
 if df.isnull().any(axis=None):
     print("\nPreview of data with null values:\nxxxxxxxxxxxxx")
     print(df[df.isnull().any(axis=1)].head(3))
     missingno.matrix(df)
     plt.show()
-    
-#Checking if there are duplicated entries
+
+# Checking if there are duplicated entries
 if len(df[df.duplicated()]) > 0:
     print("No. of duplicated entries: ", len(df[df.duplicated()]))
-    print(df[df.duplicated(keep=False)].sort_values(by=list(df.columns)).head())
+    # print(df[df.duplicated(keep=False)].sort_values(by=list(df.columns)).head())
 else:
     print("No duplicated entries found")
 
-#Define our variable of interest
-VarofInt = 'Attrition' #the name of the column that we will be focusing on
+# Define our variable of interest
+TargetVar = 'Attrition'  # the name of the column that we will be focusing on
 
-#Running the code to get plots and preliminar information
-processing.categorical_eda(df, VarofInt)
+# Running the code to get plots and preliminar information
+processing.categorical_eda(df_categs, TargetVar)
+# processing.numerical_eda(df)
 
-#Selecting the most relevant attritubtes from the dataframe to perform further analysis
-processing.bestFeature(df, VarofInt)
+# Selecting most relevant features from dataframe to perform further analysis
+try:
+    print("Select KBest with Mutual Info Classifier:")
+    processing.bestFeature_MutualInfoClassif(df, TargetVar)
+    print("\nSelect features based on Tree Classifier:")
+    processing.bestFeature_ExtraTreesClassif(df, TargetVar)
+    print("\nSelect features based on KBest and Chi2:")
+    processing.bestFeature_KBest_chi2(df, TargetVar)
+except Exception as e:
+    print(e.message, e.args)
+
+# Preparing data for training and testing
+X = df.iloc[:, 1:]
+y = df[TargetVar]  # target column -> i.e. Attrition
+# split into train and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33)
+# feature selection
+# what are scores for the features
+"""
+for i in range(len(fs.scores_)):
+    print('Feature %d: %f' % (i, fs.scores_[i]))
+# plot the scores
+plt.bar([i for i in range(len(fs.scores_))], fs.scores_)
+plt.show()
+"""
--- processingAttributes.py
+++ processingAttributes.py
@@ -2,63 +2,76 @@
 """
 Created on Sat May  8 15:46:13 2021
 
-@author: Jonathan A. Yepez M. 
+@author: Jonathan A. Yepez M.
 """
-#File to process variables in EDA
+# File to process variables in EDA
 
-#Importing relevant libraries
+# Importing relevant libraries
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 from sklearn.feature_selection import mutual_info_classif, SelectKBest, chi2
+from sklearn.ensemble import ExtraTreesClassifier
 from functools import partial
 
-#Defining the function for Categorical variables
-def categorical_eda(df, VarOfInt):
+
+def categorical_eda(df, VarOfInt):  # function for Categorical variables
     """Given dataframe, generate EDA of categorical data"""
     print("To check: Unique count of non-numeric data")
     print(df.select_dtypes(include=['category']).nunique())
     # Plot count distribution of categorical data
     for col in df.select_dtypes(include='category').columns:
-        if col!= 'Attrition':
+        if col != 'Attrition':
             fig = sns.catplot(x=col, kind="count", hue=VarOfInt, data=df)
             fig.set_xticklabels(rotation=45)
             plt.show()
-        
-#Defining the function for Numerical variables
 
 
-#Feature selection for future analysis
-def bestFeature(df, VarOfInt):
-    X = df.iloc[:,1:]
-    Y = df[VarOfInt] #target column -> i.e. Attrition
-    #apply SelectKBest class to extract top 10 best features
-    discrete_feat_idx = [2, 4, 7, 9, 13, 15, 19] # an array with indices of discrete features
-    score_func = partial(mutual_info_classif, discrete_features=discrete_feat_idx)
-    print(score_func)
+def numerical_eda(df):  # function for Numerical variables
+    # sns.pairplot(data=df)
+    # plt.show()
+    # plot the correlation matrix of salary, balance and age in data dataframe.
+    sns.heatmap(df.corr(), annot=True, cmap='Reds')
+    plt.show()
+
+# Feature selection for future analysis
+
+
+def bestFeature_MutualInfoClassif(df, VarOfInt):
+    X = df.iloc[:, 1:]
+    y = df[VarOfInt]  # target column -> i.e. Attrition
+    score_func = partial(mutual_info_classif)
     bestfeatures = SelectKBest(score_func, k=10)
-    fit = bestfeatures.fit(X,Y)
+    fit = bestfeatures.fit(X, y)
     dfscores = pd.DataFrame(fit.scores_)
     dfcolumns = pd.DataFrame(X.columns)
-    #concat two dataframes for better visualization 
-    featureScores = pd.concat([dfcolumns,dfscores],axis=1)
-    featureScores.columns = ['Specs','Score']  #naming the dataframe columns
-    print(featureScores.nlargest(10,'Score'))  #print 10 best features
+    # concat two dataframes for better visualization
+    featureScores = pd.concat([dfcolumns, dfscores], axis=1)
+    featureScores.columns = ['Specs', 'Score']  # naming the dataframe columns
+    print(featureScores.nlargest(10, 'Score'))  # print 10 best features
 
 
-"""
-# Feature Importance with Extra Trees Classifier
-from pandas import read_csv
-from sklearn.ensemble import ExtraTreesClassifier
-# load data
-url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/pima-indians-diabetes.csv"
-names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
-dataframe = read_csv(url, names=names)
-array = dataframe.values
-X = array[:,0:8]
-Y = array[:,8]
-# feature extraction
-model = ExtraTreesClassifier(n_estimators=10)
-model.fit(X, Y)
-print(model.feature_importances_)
-"""
+def bestFeature_KBest_chi2(df, VarOfInt):
+    X = df.iloc[:, 1:]
+    y = df[VarOfInt]
+    fs = SelectKBest(score_func=chi2, k='all')
+    fit = fs.fit(X, y)
+    dfscores = pd.DataFrame(fit.scores_)
+    dfcolumns = pd.DataFrame(X.columns)
+    # concat two dataframes for better visualization
+    featureScores = pd.concat([dfcolumns, dfscores], axis=1)
+    featureScores.columns = ['Specs', 'Score']  # naming the dataframe columns
+    print(featureScores.nlargest(10, 'Score'))  # print 10 best features
+
+
+def bestFeature_ExtraTreesClassif(df, VarOfInt):
+    X = df.iloc[:, 1:]  # independent columns
+    y = df[VarOfInt]  # target column i.e price range
+    model = ExtraTreesClassifier()
+    model.fit(X, y)
+    print(model.feature_importances_)
+    # inbuilt class feature_importances-tree classifiers
+    # plot graph of feature importances for better visualization
+    feat_importances = pd.Series(model.feature_importances_, index=X.columns)
+    feat_importances.nlargest(10).plot(kind='barh')
+    plt.show()
